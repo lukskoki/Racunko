ecf4d0da3edab0f8fe4c8b4a278ac8e061f5ef88
lukskoki <luka.kovacevic141@gmail.com>
2025-10-23 20:10:50 +0200

    popravio issue da se moze swipe-anjem vratit na login kad se vec uslo u home tab

--- src/frontend/app/_layout.tsx
+++ src/frontend/app/_layout.tsx
@@ -2,8 +2,14 @@ import { Stack } from "expo-router";
 import "../global.css";
 import React from "react";
 export default function RootLayout() {
-    return <Stack>
-        <Stack.Screen name="tabs" options={{headerShown: false}} />
+    return <Stack screenOptions={{ gestureEnabled: true }}>
+        <Stack.Screen
+            name="tabs"
+            options={{
+                headerShown: false,
+                gestureEnabled: false, // S ovim onemogucujemo da se moze swipeat da bi se vratilo iz home nazad na registraciju i login
+            }}
+        />
         <Stack.Screen name="index" options={{headerShown: false}} />
         <Stack.Screen name="login-signup" options={{headerShown: false}} />
     </Stack>;
