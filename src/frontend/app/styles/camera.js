ebd5ae7bb9a45989e538be75b2a45284c1ebd85d
lmajcen196 <lm55548@fer.hr>
2025-10-23 17:48:58 +0200

    Saving camera tab

---
+++ src/frontend/app/styles/camera.js
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+        width: '100%',
+        height:'12%',
+
+    },
+
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    shutterButton: {
+        height: 90,
+        width: 90,
+
+    },
+    flipButton: {
+        height: 45,
+        width: 45,
+    }
+})
---
+++ src/frontend/app/tabs/camera-tab/camera-tab.tsx
@@ -0,0 +1,104 @@
+import {Camera, CameraType, CameraView, useCameraPermissions} from "expo-camera";
+import {Button, Pressable, View, Text, TouchableOpacity, Alert, Linking, AppState,Image} from "react-native";
+import {useCallback, useEffect, useRef, useState} from "react";
+import styles from "../../styles/camera"
+import {useFocusEffect, useNavigation} from "expo-router";
+import {images} from "@/app/assets";
+import React from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {text} from "node:stream/consumers";
+
+
+const Skeniraj = () => {
+    const [permission, requestPermission ] = useCameraPermissions();
+    const [facing, setFacing] = useState<CameraType>('back');
+    const navigation = useNavigation<any>();
+    const comingFromSettings = useRef(false);
+    const alertShown = useRef(false);
+    const cameraRef = useRef<CameraView>(null);
+
+
+    useFocusEffect(
+        useCallback(() => {
+            if(!permission) return;
+            if(!permission.granted){
+                if(permission.canAskAgain){
+                    requestPermission();
+                }else{
+                    alertShown.current = true;
+                    Alert.alert(
+                        "Permission needed",
+                        "Camera access denied. To use the Scan option enable permissions in the Settings.",
+                        [{text: "Open Settings", onPress: () => {
+                                comingFromSettings.current = true;
+                                Linking.openSettings();
+                            }},
+                            {text: "Cancel", onPress: () => {navigation.navigate("home-tab")
+                                }}
+                        ],
+
+                    )
+                }
+
+            }
+        }, [permission,requestPermission,navigation])
+
+    );
+
+
+    useFocusEffect(() => {
+        const sub = AppState.addEventListener("change", async (state) =>{
+            if(state === "active" && comingFromSettings.current){
+                navigation.navigate("home-tab");
+            }
+        })
+    })
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+    async function takePicture() {
+        try {
+            const result = await cameraRef.current?.takePictureAsync({
+                quality: 1,
+                base64: true,
+                skipProcessing: true,
+            });
+            if(result?.uri){
+                Alert.alert(
+                    "Picture taken",
+                    result?.uri ?? "No such picture taken",
+                    [{text: "Ok", onPress: () => navigation.navigate("camera-tab")}],
+                )
+            }
+
+        }catch(error){
+            console.log("Error taking picture:", error);
+        }
+    }
+    if(!permission) return;
+    if(permission.granted){
+        return (
+            <View style={styles.container}>
+                <CameraView style={styles.camera} facing={facing} ref={cameraRef}/>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={toggleCameraFacing} >
+                        <Image source={images.thumbnail} style={styles.flipButton}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={takePicture} >
+                        <Image source={images.shutter} style={styles.shutterButton}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={toggleCameraFacing}>
+                        <Image source={images.flipCamera} style={styles.flipButton}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+
+
+}
+
+export default Skeniraj;
