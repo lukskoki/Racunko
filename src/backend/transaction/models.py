9d89386f5d737b548b3aea6ddc224338961ea7a0
lukskoki <luka.kovacevic141@gmail.com>
2025-10-24 21:51:43 +0200

    dodatno preimenovao neka polja

--- src/backend/transaction/models.py
+++ src/backend/transaction/models.py
@@ -24,12 +24,12 @@ class Transaction(models.Model):
     amount = models.DecimalField() # Mora bit decimal, a ne integer
     date = models.DateTimeField(auto_now_add=True) #dodao auto_now_add da vidimo kada je napravljena instanca
     # Category Id mora bit foreignKey
-    categoryID = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name="transactions") #on_delete=models.SET_NULL ce postaviti taj categoryID na null
-    groupID = models.ForeignKey(Group, on_delete=models.PROTECT, related_name="transactions", blank=True, null=True)  # dodao da je groupID foreign key, ista sema kao username samo optional
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, related_name="transactions") #on_delete=models.SET_NULL ce postaviti taj categoryID na null
+    group = models.ForeignKey(Group, on_delete=models.PROTECT, related_name="transactions", blank=True, null=True)  # dodao da je groupID foreign key, ista sema kao username samo optional
     image = models.CharField(max_length=255, blank=True, null=True)
     transactionNote = models.CharField(max_length=255, blank=True, null=True)
     # Store id mora bit Foreign Key
-    storeID = models.ForeignKey(Store, on_delete=models.SET_NULL, blank=True, null=True, related_name="transactions")
+    store = models.ForeignKey(Store, on_delete=models.SET_NULL, blank=True, null=True, related_name="transactions")
 
     def __str__(self):        
         return self.transactionID
