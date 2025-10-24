919d9131572429a9eaff5e257ccf53b13efb1199
Timon <timon.menalo@yahoo.com>
2025-10-24 13:13:23 +0200

    Nove ikone u tabs-u

--- src/frontend/app/tabs/_layout.tsx
+++ src/frontend/app/tabs/_layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Tabs} from "expo-router";
-
-
-import Feather from '@expo/vector-icons/Feather';
+import {Ionicons} from "@expo/vector-icons";
 
 export default function TabLayout() {
 
@@ -10,7 +8,7 @@ export default function TabLayout() {
         <Tabs
             screenOptions={{
                 headerShown: false,
-                tabBarStyle: {borderWidth: 2, borderTopRightRadius: 15, borderTopLeftRadius: 15, height: 90,  width: '100%', shadowColor: "#3e3d3d", shadowOpacity: 0.7,
+                tabBarStyle: {borderWidth: 2, borderTopRightRadius: 15, borderTopLeftRadius: 15, height: 85,  width: '100%', shadowColor: "#3e3d3d", shadowOpacity: 0.7,
                     shadowOffset:{width:0, height:-4},shadowRadius:2, elevation:15},
                 tabBarLabelStyle: {fontSize: 11, fontWeight: "thin"},
                 tabBarItemStyle: {justifyContent: "center", alignItems: "center", paddingTop: 10},
@@ -18,46 +16,58 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="home-tab"
                 options={{
-                    title: 'Početna',
-                    tabBarIcon: ({ color }) => <Feather name="home" size={24} color="black" />,
-
+                    title: '',
+                    tabBarIcon: ({ focused }) => <Ionicons
+                    name={focused ? "home" : "home-outline"}
+                    size={30}
+                    color={focused ? "#2563EB" : "#888"} />,
                     headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="group-tab"
                 options={{
-                    title: 'Grupa',
-                    tabBarIcon: ({ color }) => <Feather name="award" size={24} color="black" />,
+                    title: '',
+                    tabBarIcon: ({ focused }) => <Ionicons
+                    name={focused ? "people" : "people-outline"}
+                    size={30}
+                    color={focused ? "#2563EB" : "#888"} />,
                     headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="camera-tab"
                 options={{
-                    title: 'Skeniraj',
-                    tabBarIcon: ({ color }) => <Feather name="plus" size={24} color="black" />,
+                    title: '',
+                    tabBarIcon: ({ focused }) => <Ionicons
+                        name="add"
+                        size={30}
+                        color={focused ? "#2563EB" : "#888"}/>,
                     headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="chatbot-tab"
                 options={{
-                    title: 'ChatBot',
-                    tabBarIcon: ({ color }) => <Feather name="home" size={24} color="black" />,
+                    title: '',
+                    tabBarIcon: ({ focused }) => <Ionicons
+                        name={focused ? "chatbubbles" : "chatbubbles-outline"}
+                        size={30}
+                        color={focused ? "#2563EB" : "#888"}/>,
                     headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="profile-tab"
                 options={{
-                    title: 'Profil',
-                    tabBarIcon: ({ color }) => <Feather name="home" size={24} color="black" />,
+                    title: '',
+                    tabBarIcon: ({ focused }) => <Ionicons
+                        name={focused ? "person" : "person-outline"}
+                        size={30}
+                        color={focused ? "#2563EB" : "#888"}/>,
                     headerShown: false,
                 }}
             />
-
-
         </Tabs>
     );
 }
