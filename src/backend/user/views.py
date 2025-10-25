004881b7717fda3711aa40b3b8a750e87a030c44
lukskoki <luka.kovacevic141@gmail.com>
2025-10-25 08:58:48 +0200

    Dodao login
    login je na /api/auth/login/

--- src/backend/user/views.py
+++ src/backend/user/views.py
@@ -5,16 +5,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 from .models import Profile
-
-
+from django.contrib.auth import authenticate
 @api_view(['POST'])
 @permission_classes([AllowAny])  # Dopusti bilo kome da se registrira
 def register(request):
 
     serializer = UserSerializer(data=request.data)
 
-    if serializer.is_valid():
-        # Kreiraj usera
+    if serializer.is_valid(): # Provjera dal su svi podaci dobri
+        # Kreiraj usera 
         user = serializer.save()
 
         # Kreiraj token za usera, ovo koristimo za autentikaciju u buducim requestovima
@@ -39,3 +38,42 @@ def register(request):
         }, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])  # Dopusti bilo kome da se ulogira
+def login(request):
+    # Izvucemo username i password iz requesta
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    if not username or not password:
+        return Response(
+            {"error": "Username i password ne smiju bit prazni"},
+            status = status.HTTP_400_BAD_REQUEST
+        )
+    
+    # Provjerimo dal user postoji
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        return Response(
+            {"error": "Neispravni podaci"},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    # Dobimo token za usera
+    token, created = Token.objects.get_or_create(user=user)
+
+    return Response({
+        'token': token.key, # Ovo spremimo na frontendu
+        'user': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        }
+    }, status=status.HTTP_200_OK)
