a70bbe52574e2b3777cadc276845db1c324315ce
lukskoki <luka.kovacevic141@gmail.com>
2025-11-02 18:30:24 +0100

    Dodao Category admin i popravio register
    Kategorije se mogu editat na /admin

--- src/backend/transaction/admin.py
+++ src/backend/transaction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Store, Transaction, Expense
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'categoryName')
+    search_fields = ('categoryName',)
+    ordering = ('id',)
