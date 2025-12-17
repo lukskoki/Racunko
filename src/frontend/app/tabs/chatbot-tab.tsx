38568e9e42352df35e56e7f3e4d0441e1868a309
lukskoki <luka.kovacevic141@gmail.com>
2025-12-17 17:42:14 +0100

    popravio scroll glitch

--- src/frontend/app/tabs/chatbot-tab.tsx
+++ src/frontend/app/tabs/chatbot-tab.tsx
@@ -86,7 +86,7 @@ const ChatbotTab = () => {
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
                 keyboardVerticalOffset={100}>
-                
+                    
                     <View style={styles.chatContainer}>
                         {showHistory ? (
                             historyItems.length === 0 ? (
@@ -146,8 +146,6 @@ const ChatbotTab = () => {
                             </>
                         )}
                     </View>
-                
-
                 {!showHistory && (
                     <View style={styles.inputContainer}>
                         <TextInput
