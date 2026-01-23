95fbd70c921020c2c8465baea054639789fa6f27
lukskoki <luka.kovacevic141@gmail.com>
2026-01-23 11:21:17 +0100

    popravio prompt za ai da valute budu u EUR

--- src/backend/chatbot/utils/ai_chat_client.py
+++ src/backend/chatbot/utils/ai_chat_client.py
@@ -14,7 +14,7 @@ client = OpenAI(api_key=api_key)  # Inicijaliziramo OpenAI model
 
 SYSTEM_MESSAGE = {
     "role": "system",
-    "content": "The response must be in Croatian and use Markdown format.",
+    "content": "The response must be in Croatian and use Markdown format. Also write currency in EURO.",
 }
 
 def ai_chat(message_or_messages, transactions, expenses):
